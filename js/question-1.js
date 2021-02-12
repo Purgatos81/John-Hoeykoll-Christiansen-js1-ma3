dbd023f88710540616895558aa1b68b437fe4e17
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-02-12 16:11:22 +0100

    Started and finished 1. question

---
+++ js/question-1.js
@@ -0,0 +1,11 @@
+// Question 1
+// Convert the function below to an arrow function:
+
+
+// function division(a, b) {    
+//     return a % b;
+// }
+
+const division = (a, b) => a % b;
+
+//console.log("The answer is: " + division(85,43));
---
+++ js/question-2.js
@@ -0,0 +1,5 @@
+//Question 2
+
+const GamesAPI = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+console.log(GamesAPI);
