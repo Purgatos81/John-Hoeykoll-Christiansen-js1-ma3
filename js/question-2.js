3a05d37eeb93e4ae4af363ad6c452e1885c30d71
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-02-13 13:31:13 +0100

    Almost done
    Needs loading styling and general tweeks

--- js/question-2.js
+++ js/question-2.js
@@ -4,27 +4,27 @@ const GamesAPI = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&orde
 const GamesContainer = document.querySelector(".games-container");
 GamesContainer.innerHTML = "Loading..";
 
-async function listGames() {
+async function listGames() {   
+    try {
+        GamesContainer.innerHTML = "";
+        const response = await fetch(GamesAPI);   
+        const listed = await response.json();   
+        const ListOfGames = listed.results;
+
+        for(let i = 0; i < 8; i++) {
+            console.log(ListOfGames[i].name);
     
-    GamesContainer.innerHTML = "";
-    const response = await fetch(GamesAPI);
-
-    const listed = await response.json();
-
-    const ListOfGames = listed.results;
-
-    //console.log(ListOfGames);
-
-    for(let i = 0; i < 8; i++) {
-        console.log(ListOfGames[i].name);
-
-        GamesContainer.innerHTML += `<div><h2>GAME: ${ListOfGames[i].name}</h2>
-        <p>Rating: ${ListOfGames[i].rating}</p>
-        <p>Number of tags: ${ListOfGames[i].tags.length}</p>
-        </div>
-        `
+            GamesContainer.innerHTML += `<div><h2>GAME: ${ListOfGames[i].name}</h2>
+            <p>Rating: ${ListOfGames[i].rating}</p>
+            <p>Number of tags: ${ListOfGames[i].tags.length}</p>
+            </div>
+            `
+        }
     }
-
+    catch(error) {
+        console.log("error occured ", error);
+        GamesContainer.innerHTML = `<h2 style="color: red;">Sorry, something went wrong ${error}</h2>`;
+    }
 }
 
 try {
