a96e3de735d581b01fc4bf0da6e0cc72bec55d34
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-02-13 13:19:36 +0100

    Mostly done.
    Needs error handling and tweeks

--- js/question-2.js
+++ js/question-2.js
@@ -1,5 +1,42 @@
 //Question 2
 
 const GamesAPI = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const GamesContainer = document.querySelector(".games-container");
+GamesContainer.innerHTML = "Loading..";
 
-console.log(GamesAPI);
+async function listGames() {
+    
+    GamesContainer.innerHTML = "";
+    const response = await fetch(GamesAPI);
+
+    const listed = await response.json();
+
+    const ListOfGames = listed.results;
+
+    //console.log(ListOfGames);
+
+    for(let i = 0; i < 8; i++) {
+        console.log(ListOfGames[i].name);
+
+        GamesContainer.innerHTML += `<div><h2>GAME: ${ListOfGames[i].name}</h2>
+        <p>Rating: ${ListOfGames[i].rating}</p>
+        <p>Number of tags: ${ListOfGames[i].tags.length}</p>
+        </div>
+        `
+    }
+
+}
+
+try {
+    listGames();
+}
+catch(error) {
+    console.log("error occured ", error);
+    GamesContainer.innerHTML = `<h2 style="color: red;">Sorry, something went wrong ${error}</h2>`;
+}
+
+
+
+
+
+
