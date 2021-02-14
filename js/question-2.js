2d3162f229579cad1bfba7d1e14f499b1edfc500
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-02-14 13:33:18 +0100

    Finished!

--- js/question-2.js
+++ js/question-2.js
@@ -2,7 +2,7 @@
 
 const GamesAPI = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const GamesContainer = document.querySelector(".games-container");
-GamesContainer.innerHTML = "Loading..";
+
 
 async function listGames() {   
     try {
@@ -12,8 +12,7 @@ async function listGames() {
         const ListOfGames = listed.results;
 
         for(let i = 0; i < 8; i++) {
-            console.log(ListOfGames[i].name);
-    
+            //console.log(ListOfGames[i].name);
             GamesContainer.innerHTML += `<div><h2>GAME: ${ListOfGames[i].name}</h2>
             <p>Rating: ${ListOfGames[i].rating}</p>
             <p>Number of tags: ${ListOfGames[i].tags.length}</p>
